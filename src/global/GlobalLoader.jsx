1db87b3505f444dabf181a8905c4d9ffa8509acc
MarkoP00 <marebrat789@gmail.com>
2024-09-23 20:52:20 +0200

    BUGFIX: Spinner bugfix

--- src/global/GlobalLoader.jsx
+++ src/global/GlobalLoader.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import loadingSpinner from "../assets/logo/loading-spinner.svg";
 export default function GlobalLoader() {
   return (
     <div className="fixed top-0 left-0 h-screen w-full flex justify-center items-center bg-black/30 z-[70]">
-      <img src="/loading-spinner.svg" alt="" className="w-64 h-64" />
+      <img
+        src={loadingSpinner}
+        alt=""
+        className="w-64 h-64"
+      />
     </div>
   );
 }
